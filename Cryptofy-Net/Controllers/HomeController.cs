db2e1d9673eafd37d77a832b6cd5c7c603af701a
christianrd <me@christiandr.com>
2018-04-29 20:50:38 -0400

    template implemented

--- Cryptofy-Net/Controllers/HomeController.cs
+++ Cryptofy-Net/Controllers/HomeController.cs
@@ -22,13 +22,6 @@ namespace Cryptofy_Net.Controllers
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- Cryptofy-Net/wwwroot/js/site.js
+++ Cryptofy-Net/wwwroot/js/site.js
@@ -1,1 +1,7 @@
 ﻿// Write your JavaScript code.
+
+$(function () {
+    if ($('.currency-thumbnail-content__img').size() > 0) {
+        $.adaptiveBackground.run();
+    }
+});
